7588d24dc8294472b0c87610065eddca40f2740b
december-ok <checkpoint1209@gmail.com>
2025-07-06 15:07:31 +0000

    HTTP 1.1 에서 SSL 연결 가능하도록 구현

--- main.c
+++ main.c
@@ -8,13 +8,13 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define LISTEN_PORT 8888
-
 typedef struct proxy_conn
 {
 	struct bufferevent *client_bev;
 	struct bufferevent *server_bev;
 	struct evbuffer *request_buffer;
+	char host[256];
+	int port;
 	int connected;
 	int ssl;
 } proxy_conn_t;
@@ -28,6 +28,7 @@ void close_connection(proxy_conn_t *conn)
 		bufferevent_free(conn->server_bev);
 	if (conn->request_buffer)
 		evbuffer_free(conn->request_buffer);
+
 	free(conn);
 }
 
@@ -86,10 +87,7 @@ void client_read_cb(struct bufferevent *bev, void *ctx)
 
 	if (!conn->connected)
 	{
-		// 요청 버퍼에 저장
 		evbuffer_add_buffer(conn->request_buffer, input);
-
-		// 전체 헤더를 다 받았는지 확인 (\r\n\r\n으로 끝나는지)
 		size_t len = evbuffer_get_length(conn->request_buffer);
 		unsigned char *data = evbuffer_pullup(conn->request_buffer, len);
 		char *header_end = strstr((char *)data, "\r\n\r\n");
@@ -99,45 +97,85 @@ void client_read_cb(struct bufferevent *bev, void *ctx)
 		// 메소드 파싱
 		char method[8];
 		sscanf((char *)data, "%7s", method);
+
+		// CONNECT 처리
 		if (strcmp(method, "CONNECT") == 0)
 		{
+			sscanf((char *)data, "CONNECT %255[^:]%*[:]%d", conn->host, &conn->port);
+			if (conn->port == 0)
+				conn->port = 443;
+
+			printf("[HTTPS] %s:%d\n", conn->host, conn->port);
+
+			// 서버와 연결
+			struct addrinfo hints, *res;
+			char port_str[6];
+			snprintf(port_str, sizeof(port_str), "%d", conn->port);
+
+			memset(&hints, 0, sizeof(hints));
+			hints.ai_family = AF_INET;
+			hints.ai_socktype = SOCK_STREAM;
+
+			if (getaddrinfo(conn->host, port_str, &hints, &res) != 0)
+			{
+				fprintf(stderr, "DNS failed\n");
+				close_connection(conn);
+				return;
+			}
+
+			conn->server_bev = bufferevent_socket_new(bufferevent_get_base(bev), -1, BEV_OPT_CLOSE_ON_FREE);
+			bufferevent_setcb(conn->server_bev, server_read_cb, NULL, event_cb, conn);
+			bufferevent_enable(conn->server_bev, EV_READ | EV_WRITE);
+
+			if (bufferevent_socket_connect(conn->server_bev, res->ai_addr, res->ai_addrlen) < 0)
+			{
+				fprintf(stderr, "Connection to server failed\n");
+				freeaddrinfo(res);
+				close_connection(conn);
+				return;
+			}
+
+			freeaddrinfo(res);
+			conn->connected = 1;
 			conn->ssl = 1;
-		}
 
-		// Host 헤더 파싱
-		char *host_header = strstr((char *)data, "Host:");
-		if (!host_header)
+			// 클라이언트에 200 응답 보내고, 이제부터는 raw 터널링 시작
+			bufferevent_write(conn->client_bev, "HTTP/1.1 200 Connection established\r\n\r\n", 39);
+			evbuffer_drain(conn->request_buffer, len);
+		}
+		else
 		{
-			fprintf(stderr, "No Host header found\n");
-			close_connection(conn);
-			return;
+
+			// Host 헤더 파싱
+			char *host_header = strstr((char *)data, "Host:");
+			if (!host_header)
+			{
+				fprintf(stderr, "No Host header found\n");
+				close_connection(conn);
+				return;
+			}
+			host_header += 5; // skip "Host:"
+			while (*host_header == ' ')
+				host_header++;
+
+			sscanf(host_header, "%255s", conn->host);
+
+			// 포트 포함되었는지 확인
+			char *colon = strchr(conn->host, ':');
+			if (colon)
+			{
+				*colon = '\0';
+				conn->port = atoi(colon + 1);
+			}
+			else
+			{
+				conn->port = 80;
+			}
+
+			printf("[HTTP ] %s:%d\n", conn->host, conn->port);
+
+			connect_to_server(conn, conn->host, conn->port);
 		}
-		host_header += 5; // skip "Host:"
-		while (*host_header == ' ')
-			host_header++;
-		char host[256];
-		int port = 80;
-
-		sscanf(host_header, "%255s", host);
-
-		// 포트 포함되었는지 확인
-		char *colon = strchr(host, ':');
-		if (colon)
-		{
-			*colon = '\0';
-			port = atoi(colon + 1);
-		}
-
-		if (conn->ssl)
-		{
-			printf("Unsupported SSL: %s\n", host);
-			close_connection(conn);
-			return;
-		}
-
-		printf("Connecting to server: %s:%d\n", host, port);
-
-		connect_to_server(conn, host, port);
 	}
 	else
 	{
@@ -163,11 +201,22 @@ void accept_cb(struct evconnlistener *listener, evutil_socket_t fd,
 	bufferevent_enable(conn->client_bev, EV_READ | EV_WRITE);
 }
 
-int main()
+int main(int argc, char **argv)
 {
 	struct event_base *base;
 	struct evconnlistener *listener;
 	struct sockaddr_in sin;
+	int LISTEN_PORT = 8888;
+
+	if (argc > 1)
+	{
+		LISTEN_PORT = atoi(argv[1]);
+		if (LISTEN_PORT <= 0 || LISTEN_PORT > 65535)
+		{
+			fprintf(stderr, "Invalid port number: %s\n", argv[1]);
+			return 1;
+		}
+	}
 
 	base = event_base_new();
 
